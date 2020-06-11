1a0877e140288eb7f1028502e973e363dde54950
Zush18 <zzush18@gmail.com>
2020-06-11 00:32:49 -0500

    Vector generic checkpoint

---
+++ Vectors/Vector/Generic/libgenericvector.c
@@ -0,0 +1,35 @@
+// Include local
+#include "GenericVector.h"
+#include <lib/util.h>
+#include <lib/validate.h>
+
+// Include standar
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//Struct
+struct GenericVector_Struct {
+	int length;
+	void* array;
+};
+
+typedef struct GenericVector_Struct GenericVector;
+
+//Create & destroy
+void initGenericVector(GenericVector * generic_vector, int length, int size){
+	valNeg(size);
+	generic_vector->length = length;
+	generic_vector->array = (int*) calloc(length, size);
+	valPtr(generic_vector->array);
+}
+
+void initGenericVectorPointer(GenericVector** generic_vector, int length, int size){
+	*generic_vector = (GenericVector*) calloc(1, sizeof(GenericVector));
+	initGenericVector(*generic_vector, length, size);
+}
+
+void destroyGenericVector(GenericVector * generic_vector){
+	free(generic_vector->array);
+	free(generic_vector);
+}
